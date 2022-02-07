3c22bd118c6f19baf2453160264774b981887dd0
Terencio Martins <zerofirty0@gmail.com>
2022-02-07 03:03:02 -0300

    Adicionando MayAnimeList

--- main.js
+++ main.js
@@ -38,27 +38,63 @@ arrow.addEventListener('click', () => {
 
 const pokedex = document.querySelector('.pokedex')
 pokedex.addEventListener('mouseenter',() => {
+    pokedex.className = ('pokedex')
     pokedex.classList.add('animate__animated', 'animate__infinite', 'animate__pulse')
 })
 pokedex.addEventListener('mouseleave', () => {
     pokedex.classList.remove('animate__animated', 'animate__infinite', 'animate__pulse')
 })
 
+const animeImg = document.querySelector('.animeImg')
+
+animeImg.addEventListener('mouseenter',() => {
+    animeImg.className = ('animeImg')
+    animeImg.classList.add('animate__animated', 'animate__infinite', 'animate__pulse')
+})
+animeImg.addEventListener('mouseleave', () => {
+    animeImg.classList.remove('animate__animated', 'animate__infinite', 'animate__pulse')
+})
+
 const artigo2 = document.querySelector('.articlePokedex')
 const titulo2 = document.querySelector('.tituloPokedex')
+const artigoAnime = document.querySelector('.articleAnime')
+const tituloAnime = document.querySelector('.tituloAnime')
+const imgAnime = document.querySelector('.animeImg')
 const div = document.querySelector('.textoPokedex')
+const textoAnime = document.querySelector('.textoAnime')
 pokedex.addEventListener('click', () => {
     console.log(textUp);
     switch (textUp) {
         case false:
             document.getElementById("idPokedex").style.display = "inline-block"
             div.className = 'textoPokedex animate__animated animate__fadeInLeft'
+            imgAnime.className = "animeImg animate__animated animate__fadeOutRight"
+            textUp = true;
+            break
+
+        case true:
+            div.className ='textoPokedex animate__animated animate__fadeOutLeft'
+            imgAnime.className = "animeImg animate__animated animate__fadeInRight"
+            // document.getElementById("idPokedex").style.display = "none"
+            textUp = false;
+            break
+    }
+})
+imgAnime.addEventListener('click', () => {
+    console.log(textUp);
+    switch (textUp) {
+        case false:
+            document.getElementById("idAnime").style.display = "inline-block"
+            textoAnime.className = 'textoAnime animate__animated animate__fadeInRight'
+            pokedex.className = "pokedex animate__animated animate__fadeOutLeft"
             textUp = true;
             break
 
         case true:
             
-            div.className ='textoPokedex animate__animated animate__fadeOutLeft'
+            textoAnime.className ='textoAnime animate__animated animate__fadeOutRight'
+            pokedex.className = "pokedex animate__animated animate__fadeInLeft"
+            // document.getElementById("idAnime").style.display = "none"
             textUp = false;
             break
     }
